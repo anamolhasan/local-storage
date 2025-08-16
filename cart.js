f2b3c84fa5642f7ba1519da3854e8e2f861c4c33
anamolhasan <mdanamolhasan1998@gmail.com>
2025-08-16 19:45:30 +0600

    local storage cart

---
+++ cart.js
@@ -0,0 +1,49 @@
+const handleAddToCart = () => {
+    const productName = document.getElementById('productName')
+    const productQuantity = document.getElementById('productQuantity')
+
+    const name = productName.value
+    const quantity = productQuantity.value
+
+   displayProduct(name, quantity)
+   saveProductToLocalStorage(name, quantity)
+
+
+    productName.value = ''
+    productQuantity.value = ''
+}
+
+const displayProduct = (productName, quantity) => {
+    const productContainer = document.getElementById('product-container')
+
+    const li = document.createElement('li')
+    li.innerText =  `${productName} : ${quantity}`
+
+    productContainer.appendChild(li)
+}
+
+const getProductFromLocalStorage = () => {
+    let cart = {}
+    const getProduct = localStorage.getItem('cart')
+
+    if(getProduct){
+        cart = JSON.parse(getProduct)
+    }
+    return cart
+}
+
+const displayProductFromLocalStorage = () => {
+    const products = getProductFromLocalStorage()
+    for(const product in products){
+        displayProduct(product, products[product])
+    }
+}
+
+const saveProductToLocalStorage = (productName, quantity) => {
+    const cart = getProductFromLocalStorage()
+    cart[productName] = quantity
+    const cartString = JSON.stringify(cart)
+    localStorage.setItem('cart', cartString)
+}
+
+displayProductFromLocalStorage()
