6095b606a8aa66fa1300dd517a2a367d3d954aff
anamolhasan <mdanamolhasan1998@gmail.com>
2025-08-16 13:24:42 +0600

    local storage in set,get items

--- storage.js
+++ storage.js
@@ -7,7 +7,17 @@
 // }
 
 const handleAddToStorage = () => {
-   const name = 'Anamol Hasan'
-
-   localStorage.setItem('name', name)
+   const person = {
+    name: 'anamol hasan',
+    age: 20,
+    friends: ['hasan, emon, sojon']
+   }
+   const convertedToString = JSON.stringify(person)
+   
+   localStorage.setItem('anam', convertedToString)
+ 
 }
+
+const data = localStorage.getItem('anam')
+const converted = JSON.parse(data)
+console.log(converted)
